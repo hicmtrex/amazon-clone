caa35f1e8debee5917faa87a90fabc3197009e9b
Firstname Lastname <your_email@youremail.com>
2022-08-27 14:39:35 +0100

    🧐

--- pages/login.js
+++ pages/login.js
@@ -15,7 +15,7 @@ export default function LoginScreen() {
 
   useEffect(() => {
     if (session?.user) {
-      router.push(redirect || '/');
+      router.push('/');
     }
   }, [router, session, redirect]);
 
@@ -39,16 +39,16 @@ export default function LoginScreen() {
     }
   };
   return (
-    <Layout title="Login">
+    <Layout title='Login'>
       <form
-        className="mx-auto max-w-screen-md"
+        className='mx-auto max-w-screen-md'
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Login</h1>
-        <div className="mb-4">
-          <label htmlFor="email">Email</label>
+        <h1 className='mb-4 text-xl'>Login</h1>
+        <div className='mb-4'>
+          <label htmlFor='email'>Email</label>
           <input
-            type="email"
+            type='email'
             {...register('email', {
               required: 'Please enter email',
               pattern: {
@@ -56,34 +56,36 @@ export default function LoginScreen() {
                 message: 'Please enter valid email',
               },
             })}
-            className="w-full"
-            id="email"
+            className='w-full'
+            id='email'
             autoFocus
           ></input>
           {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
+            <div className='text-red-500'>{errors.email.message}</div>
           )}
         </div>
-        <div className="mb-4">
-          <label htmlFor="password">Password</label>
+        <div className='mb-4'>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
+            type='password'
             {...register('password', {
               required: 'Please enter password',
               minLength: { value: 6, message: 'password is more than 5 chars' },
             })}
-            className="w-full"
-            id="password"
+            className='w-full'
+            id='password'
             autoFocus
-          ></input>
+          />
           {errors.password && (
-            <div className="text-red-500 ">{errors.password.message}</div>
+            <div className='text-red-500 '>{errors.password.message}</div>
           )}
         </div>
-        <div className="mb-4 ">
-          <button className="primary-button">Login</button>
+        <div className='mb-4 '>
+          <button type='submit' className='primary-button'>
+            Login
+          </button>
         </div>
-        <div className="mb-4 ">
+        <div className='mb-4 '>
           Don&apos;t have an account? &nbsp;
           <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
         </div>
